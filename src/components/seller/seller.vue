fb2d1b74ed1ba1176fe7b2922a7fa874cb411a77
IdenPin <635639654@qq.com>
2017-04-25 21:27:03 +0800

    codding for seller

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,13 +1,177 @@
 <template lang="html">
-  <div class="">
-    seller
+  <div class="seller">
+    <div class="seller-wrapper">
+      <div class="seller-info border-1px">
+        <div class="name-and-star">
+          <h1 class="name">{{seller.name}}</h1>
+          <div class="star">
+            star({{seller.ratingCount}})
+            <span class="sell-count">月售{{seller.sellCount}}单</span>
+          </div>
+        </div>
+        <div class="collect">
+          <span class="icon-favorite"></span>
+          <span class="text">收藏</span>
+        </div>
+      </div>
+      <div class="seller-delivery">
+        <div class="itme">
+          <div class="text">起送价</div>
+            <span class="unit">{{seller.minPrice}}<span class="pre">元</span></span>
+        </div>
+        <div class="itme">
+            <div class="text">商家配送</div>
+            <span class="unit">{{seller.deliveryPrice}}<span class="pre">元</span></span>
+        </div>
+        <div class="itme">
+            <div class="text">平均配送时间</div>
+            <span class="unit">{{seller.deliveryTime}}<span class="pre">分钟</span></span>
+        </div>
+      </div>
+    </div>
+  <split></split>
+  <div class="seller-com seller-bulletins">
+    <h1 class="title">公告与活动</h1>
+    <div class="content">
+      <div class="warm-tip">
+        {{seller.bulletin}}
+      </div>
+      <ul class="activity-type">
+        <li v-for="(v,index) in seller.supports" class="border-1px">
+          <span class="text">{{v.description}}</span>
+        </li>
+      </ul>
+    </div>
   </div>
+</div>
 </template>
-
 <script>
-export default {
-}
+  import split from '../split/split'
+  const ERR_OK = 0
+
+  export default {
+    data() {
+      return {
+        seller: {}
+      }
+    },
+    created() {
+      this.$http.get('/api/seller').then((res) => {
+        if (res.body.errno === ERR_OK) {
+          this.seller = res.body.data
+        }
+      })
+    },
+    components: {
+      split
+    }
+  }
 </script>
 
-<style lang="css">
+<style lang="scss">
+  @import "../../common/style/mixin";
+  .seller {
+    .seller-wrapper {
+      padding: 18px;
+      .seller-info {
+        display: flex;
+        @include border-1px(rgba(7, 17, 27, 0.1));
+        .name-and-star {
+          flex: 1;
+          .name {
+            font-size: 14px;
+            font-weight: 500;
+            line-height: 14px;
+            color: rgb(7, 17, 27);
+          }
+          .star {
+            margin-top: 8px;
+            margin-bottom: 18px;
+            color: rgb(77, 85, 93);
+            font-size: 10px;
+            .sell-count {
+              margin-left: 12px;
+            }
+          }
+        }
+        .collect {
+          flex: 0 0 30px;
+          width: 30px;
+          text-align: center;
+          .icon-favorite {
+            font-size: 24px;
+            line-height: 24px;
+            color: #d4d6d9;
+          }
+          .text {
+            margin-top: 4px;
+            display: block;
+            font-size: 10px;
+          }
+        }
+      }
+      .seller-delivery {
+        display: flex;
+        margin-top: 18px;
+        .itme {
+          flex: 1;
+          text-align: center;
+          border-right: 1px solid rgba(7, 17, 27, .1);
+          &:last-child {
+            border-right: none;
+          }
+          .text {
+            font-size: 10px;
+            color: rgb(147, 153, 159);
+            line-height: 20px;
+            margin-bottom: 4px;
+          }
+          .unit {
+            font-size: 24px;
+            line-height: 24px;
+            font-weight: 200;
+            color: rgb(7, 17, 27);
+            .pre {
+              font-size: 12px;
+            }
+          }
+        }
+      }
+    }
+    .seller-bulletins {
+      .warm-tip{
+        font-size: 12px;
+        font-weight: 200px;
+        color: rgb(240, 20, 20);
+        line-height: 24px;
+      }
+      .activity-type{
+        li{
+          @include border-1px(rgba(7, 17, 27, 0.1));
+          padding: 16px 11px;
+          height: 16px;
+          .text{
+            font-size: 12px;
+            font-weight: 200;
+            color: rgb(7,17,27);
+          }
+          &:last-child{
+            @include border-none();
+          }
+        }
+      }
+    }
+
+    /*seller 公用*/
+    .seller-com {
+      padding: 18px;
+      .title {
+        font-size: 14px;
+        font-weight: 500;
+        line-height: 14px;
+        color: rgb(7, 17, 27);
+        margin-bottom: 8px;
+      }
+    }
+  }
 </style>
